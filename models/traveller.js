d67888b7b5a0d0f0ffc7577b2a62bc0f6e0dd9b4
cjmcauley <christopher.mcauley@gmail.com>
2019-07-31 16:15:52 +0100

    homework complete

--- models/traveller.js
+++ models/traveller.js
@@ -3,27 +3,31 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return result = this.journeys.map(journey => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return result = this.journeys.map(journey => journey.endLocation);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journeys => transport === journeys.transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journeys => journeys.distance > minDistance)
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const result = this.journeys.reduce((sum, journey) => {
+    return sum + journey.distance;
+  }, 0)
+  return result
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  allTransport = this.journeys.map(journey => journey.transport);
+  return uniqueTransport = [...new Set(allTransport)];
 };
 
 
